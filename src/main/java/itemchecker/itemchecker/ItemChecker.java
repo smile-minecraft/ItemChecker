112e85164621c699da2f25bf09c8d08845de4203
smile-minecraft <54743433+smile-minecraft@users.noreply.github.com>
2022-08-25 01:07:19 +0800

    初始化

---
+++ src/main/java/itemchecker/itemchecker/ItemChecker.java
@@ -0,0 +1,17 @@
+package itemchecker.itemchecker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemChecker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
